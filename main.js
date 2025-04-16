29db201afd110a0eb78a29c1926b52ceaa07e14a
aashishdaily <aashishdaily@gmail.com>
2025-04-16 19:11:52 +0545

    added some messages

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+
+// assinging values
+
 const addTaskButton = document.getElementById("add-task")
 const taskInput = document.getElementById("task-input")
 const taskList = document.getElementById("task-list")
 
+// adding an eventlisterer
+// when you click on addTaskbutton -> 
+// created taskText var assigned it to taskInput.value.trim()
+// this trims the todo values 
+// eg: if user typed " Learn JS freecodecamp  "
+// this command will return "Learn JS freecodecamp"
+// No spaces
+// if (taskText === "") return 
+// taskText is a var holding trimed input
+// if statements checks if the trimmed input is""
+// if it is empty it 
 addTaskButton.addEventListener("click", () =>{
     const taskText = taskInput.value.trim();
     if (taskText === "") return
